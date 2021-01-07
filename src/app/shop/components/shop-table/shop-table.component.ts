3faf26704a7703f44922ff355af2e2edc5b0f1fe
Voytek <golewski.wojciech@outlook.com>
2021-01-07 22:52:15 +0100

    generated shop module alongside initial components

---
+++ src/app/shop/components/shop-table/shop-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-table',
+  templateUrl: './shop-table.component.html',
+  styleUrls: ['./shop-table.component.scss']
+})
+export class ShopTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopRoutingModule } from './shop-routing.module';
+import { MainShopComponent } from './components/main-shop/main-shop.component';
+import { ShopTableComponent } from './components/shop-table/shop-table.component';
+
+
+@NgModule({
+  declarations: [MainShopComponent, ShopTableComponent],
+  imports: [
+    CommonModule,
+    ShopRoutingModule
+  ]
+})
+export class ShopModule { }
