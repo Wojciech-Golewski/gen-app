e318cd15d77edb147e6686e98e0df6b52fcaa96b
Voytek <golewski.wojciech@outlook.com>
2021-01-09 23:29:05 +0100

    layout module now by default and initially points to shop module main component

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LayoutComponent } from './layout.component';
-
-const routes: Routes = [{ path: '', component: LayoutComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../shop/shop.module').then((m) => m.ShopModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
