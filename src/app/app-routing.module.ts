4b5599d925e4a733a2d12268faf46ef5f5ee4bf3
Voytek <golewski.wojciech@outlook.com>
2021-01-09 23:26:10 +0100

    routing first and default to layout now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
-  },
-  {
-    path: 'layout',
     loadChildren: () =>
       import('./layout/layout.module').then((m) => m.LayoutModule),
   },
