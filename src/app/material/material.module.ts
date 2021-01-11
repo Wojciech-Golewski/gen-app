8cdb407c9c791b2ad362b52ed14b0695e2f1f662
Voytek <golewski.wojciech@outlook.com>
2021-01-11 22:49:05 +0100

    added a table for further testing

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,9 +2,32 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+  ],
 })
 export class MaterialModule {}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { MainShopComponent } from './components/main-shop/main-shop.component';
 import { ShopTableComponent } from './components/shop-table/shop-table.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [MainShopComponent, ShopTableComponent],
-  imports: [CommonModule, ShopRoutingModule],
+  imports: [CommonModule, ShopRoutingModule, MaterialModule],
 })
 export class ShopModule {}
